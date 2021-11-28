75bdfffce51536d8d27305195452b24d1cc15c70
Documenter.jl <documenter@juliadocs.github.io>
2021-11-28 07:04:05 +0000

    build based on 1afa568

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeneticsMakie","category":"page"},{"location":"#GeneticsMakie","page":"Home","title":"GeneticsMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeneticsMakie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeneticsMakie]","category":"page"}]
+}
