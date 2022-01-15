474e7d084f665253e4bef4a30c69821de8214f4a
Minsoo <mmkim1210@gmail.com>
2022-01-14 20:15:35 -0800

    add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,39 @@
 using GeneticsMakie
 using Test
+using CSV
+using CairoMakie
+using DataFrames
+using SnpArrays
+using Statistics
 
-@testset "GeneticsMakie.jl" begin
-    # Write your tests here.
+@testset "Parsing GENCODE" begin
+    gencode = CSV.read("data/gencode.gtf", DataFrame)
+    GeneticsMakie.parsegtf!(gencode)
+    @test ncol(gencode) == 14
+end
+
+@testset "Plotting genes" begin
+end
+
+@testset "Plotting isoforms" begin
+end
+
+@testset "Munging summmary stats" begin
+end
+
+@testset "Plotting LD" begin
+end
+
+@testset "Plotting LocusZoom" begin
+end
+
+@testset "Plotting GWAS" begin
+    # @test isfile("gwas.png")
+    # rm("gwas.png")
+end
+
+@testset "Plotting qq" begin
+end
+
+@testset "Plotting TWAS" begin
 end
